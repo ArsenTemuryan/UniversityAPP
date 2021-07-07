ee7d2ff0c8eb5cdec8fcc8f6755a88ccaa14f220
alex <alextemuryan@gmail.com>
2021-07-08 01:09:18 +0400

    Добавьте файлы проекта.

---
+++ UniversityAPP/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityAPP.Models;
+using UniversityAPP.Managers;
+
+namespace UniversityAPP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentManager studentmanager = new StudentManager();
+            StudentModel[] students = studentmanager.GetStudent(63);
+            TeacherManager teacherManager = new TeacherManager();
+            TeacherModel[] teachers = teacherManager.GetTeacher(4);
+            UniversityManager university = new UniversityManager();
+            TeacherModel[] swappedteachers = university.swappedTeachersWithStudents(students, teachers);
+            StudentModel[] swappesstudents = university.swappedStudentsWithTeachers(students, swappedteachers);
+        }
+    }
+}
